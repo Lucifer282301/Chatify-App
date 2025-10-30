0ec3512bd0f51a5dae5daaf231e5fcf681c744ac
Avinash Kumar <avinash.kumar@boostb2b.com>
2025-10-30 23:30:29 +0530

    [CA-006] Implemented login and logout functionality

--- server/src/controllers/authControllers.js
+++ server/src/controllers/authControllers.js
@@ -81,5 +81,60 @@ export const signupController = async (req, res) => {
   }
 };
 
-// eslint-disable-next-line no-unused-vars
-export const loginController = async (req, res) => {};
+export const loginController = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required!",
+      });
+    }
+
+    // Check if email is valid
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email format!",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials!",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials!",
+      });
+    }
+
+    generateToken(user._id, res);
+    return res.status(200).json({
+      success: true,
+      message: "User logged in successfully!",
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller: ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logoutController = (_, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  return res.status(200).json({
+    success: true,
+    message: "User logged out successfully!",
+  });
+};
--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,16 +1,14 @@
 import express from "express";
 import {
-  loginController,
   signupController,
+  loginController,
+  logoutController,
 } from "../controllers/authControllers.js";
 
 const router = express.Router();
 
 router.post("/signup", signupController);
 router.post("/login", loginController);
-
-router.get("/logout", (req, res) => {
-  res.send("User logged out");
-});
+router.post("/logout", logoutController);
 
 export default router;
