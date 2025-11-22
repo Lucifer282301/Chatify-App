fd7cbe7f28559ae66dc93679cc8c815bd4d05365
Avinash Kumar <avinash.kumar@boostb2b.com>
2025-11-22 23:00:45 +0530

    [CA-009] Implemented message route functionality

--- server/src/routes/messageRoutes.js
+++ server/src/routes/messageRoutes.js
@@ -1,9 +1,22 @@
 import express from "express";
+import {
+  getAllContacts,
+  getChatPartners,
+  getMessagesByUserId,
+  sendMessage,
+} from "../controllers/messageControllers.js";
+import { protectRoute } from "../middleware/authMiddleware.js";
+import { arcjetProtection } from "../middleware/arcjectMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Messages retrieved");
-});
+// the middlewares execute in order - so requests get rate-limited first, then authenticated.
+// this is actually more efficient since unauthenticated requests get blocked by rate limiting before hitting the auth middleware.
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessagesByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
--- server/src/utils/arcjet.js
+++ server/src/utils/arcjet.js
@@ -7,7 +7,7 @@ const aj = arcjet({
     shield({ mode: "LIVE" }),
     detectBot({
       mode: "LIVE",
-      allow: ["CATEGORY:SEARCH_ENGINE"],
+      allow: ["CATEGORY:SEARCH_ENGINE", "POSTMAN"],
     }),
     slidingWindow({
       mode: "LIVE",
