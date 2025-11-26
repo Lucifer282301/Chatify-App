52044d2124b0398f65b41963fc77bfa081fd9d9a
Avinash Kumar <avinash.kumar@boostb2b.com>
2025-11-26 23:17:30 +0530

    [CA-017] Implemented Socket.io

--- client/src/components/ChatsList.jsx
+++ client/src/components/ChatsList.jsx
@@ -2,12 +2,12 @@ import { useEffect } from "react";
 import { useChatStore } from "../store/useChatStore";
 import UsersLoadingSkeleton from "./UsersLoadingSkeleton";
 import NoChatsFound from "./NoChatsFound";
-// import { useAuthStore } from "../store/useAuthStore";
+import { useAuthStore } from "../store/useAuthStore";
 
 const ChatsList = () => {
   const { getMyChatPartners, chats, isUsersLoading, setSelectedUser } =
     useChatStore();
-  // const { onlineUsers } = useAuthStore();
+  const { onlineUsers } = useAuthStore();
 
   useEffect(() => {
     getMyChatPartners();
@@ -26,10 +26,9 @@ const ChatsList = () => {
         >
           <div className="flex items-center gap-3">
             <div
-              className={`avatar online`}
-              // className={`avatar ${
-              //   onlineUsers.includes(chat._id) ? "online" : "offline"
-              // }`}
+              className={`avatar ${
+                onlineUsers.includes(chat._id) ? "online" : "offline"
+              }`}
             >
               <div className="size-12 rounded-full">
                 <img
